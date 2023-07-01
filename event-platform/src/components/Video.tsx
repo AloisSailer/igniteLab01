c9a6abc1b5d2a301fe5b6743bc2476df63149a14
Alois Sailer <alois_sailer@yahoo.com.br>
2023-07-01 20:27:13 -0300

    continuação

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -4,6 +4,14 @@ export function Video() {
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
       </div>
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1>Aula 01 - Abertura do Ignite Lab</h1>
+            <p>/text</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
